077f3fc674a192bfe405bc5308105c1011fce0cd
abby <abby201205@gmail.com>
2022-11-04 16:33:50 +0800

    完成年份和月份的切換

--- php/note/calendar/index.php
+++ php/note/calendar/index.php
@@ -12,15 +12,15 @@
 
   <style>
     /*請在這裹撰寫你的CSS*/
-    * {
+    /* * {
       margin: 0;
       padding: 0;
       box-sizing: border-box;
-    }
+    } */
 
     .my-top {
       width: 100%;
-      height: 40px;
+      height: 50px;
       background-color: lightblue;
       justify-content: space-around;
       align-items: center;
@@ -30,7 +30,7 @@
       display: flex;
       flex-wrap: wrap;
       width: 80%;
-      height: 300px;
+      height: 400px;
       margin: auto;
     }
 
@@ -40,46 +40,67 @@
       margin-left: -1px;
       margin-top: -1px;
     }
-
+    
     .cal .date:hover {
       transform: scale(1.05);
       background-color: lightcyan;
     }
-
+    
     .holiday {
       background-color: pink;
     }
-  </style>
+    .week{
+    border: 1px solid #999;
+      width: calc(100% / 7);
+      margin-left: -1px;
+      margin-bottom: -1px;
+      height: 40px;
+      text-align: center;
+      
+    }
+    </style>
 </head>
 
 <body>
   <h1>萬年曆</h1>
+  <a href="./text.php">測試</a>
 
   <?php
   /*請在這裹撰寫你的萬年曆程式碼*/
   $cal = [];
   $holiday = ['2022-10-25' => "光復節", "2022-10-10" => "國慶日"]; // 增加特殊日期判斷
+  
   $month = (isset($_GET['m'])) ? $_GET['m'] : date("n"); //n	数字表示的月份，没有前导零
   $year = (isset($_GET['y'])) ? $_GET['y'] : date("Y"); //Y	4 位数字完整表示的年份
 
-  $nextMonth = $month + 1;
-  $prevMonth = $month - 1;
+  // $nextMonth = $month + 1;
+  // $prevMonth = $month - 1;
 
-  $firstDay = $year . "-" . $month . "-1";
+  $nextYear=$year;
+    $nextMonth=$month+1;
+    if ($nextMonth==12) {
+        $nextMonth=1;
+        $nextYear=$year+1;
+    }
+    
+    $prevYear=$year;
+    $prevMonth=$month-1;
+    if ($prevMonth==1) {
+        $prevMonth=12;
+        $prevYear=$year-1;
+    }
+
+
+  $firstDay = $year . "-" . $month . "-1"; //本月第一天
   $firstDayWeek = date("N", strtotime($firstDay)); //N	ISO-8601 格式数字表示的星期中的第几天（PHP 5.1.0 新加）	1（表示星期一）到 7（表示星期天）
   $monthDays = date("t", strtotime($firstDay)); //t 指定的月份有几天
   $lastDay = $year . '-' . $month . '-' . $monthDays;
   $spaceDays = $firstDayWeek - 1;
   $weeks = ceil(($monthDays + $spaceDays) / 7);
   $lastSpaceDays=$weeks*7-$monthDays-$spaceDays;
+  
 
-
-  $firstDay = $year . "-" . $month . "-1";
-  $firstDayWeek = date("N", strtotime($firstDay));
-  $monthDays = date("t", strtotime($firstDay));
-  $lastDay = $year . '-' . $month . '-' . $monthDays;
-  $spaceDays = $firstDayWeek - 1;
-  $weeks = ceil(($monthDays + $spaceDays) / 7);
+ 
 
   //echo 前面空白
   for ($i = 0; $i < $spaceDays; $i++) {
@@ -91,10 +112,10 @@
   }
   //m	数字表示的月份，有前导零   d 月份中的第几天，有前导零的
 
-  //後面空白
-  // for ($i=0; $i < ; $i++) { 
-  //   # code...
-  // }
+  // 後面空白
+  for ($i=0; $i < $lastSpaceDays ; $i++) { 
+    $cal[]='';
+  }
 
   /* echo "<pre>";
 print_r($cal);
@@ -106,17 +127,36 @@ echo "</pre>"; */
   echo "<br>";
   echo "月曆天數共" . ($monthDays + $spaceDays) . "天，" . $weeks . "周";
 
+
   ?>
 
-  <div class="d-flex my-top">
-    <a href="?y=2022&m=<?= $prevMonth; ?>">上一個月</a>
-    <h1 ><?= $year; ?> 年 <?= $month; ?> 月份</h1>
-    <a href="?y=2022&m=<?= $nextMonth; ?>">下一個月</a>
-  </div>
-  <br>
 
-  <div class='cal'>
-    <?php
+<!-- <div class="d-flex my-top">
+  <a href="?y=2022&m=<?= $prevMonth; ?>">上一個月</a>
+  <h1 ><?= $year; ?> 年 <?= $month; ?> 月份</h1>
+  <a href="?y=2022&m=<?= $nextMonth; ?>">下一個月</a>
+</div> -->
+
+<div class="d-flex my-top">
+  <a href="?y=<?= $prevYear?>&m=<?=$prevMonth; ?>">上一個月</a>
+  <h1 ><?= $year; ?> 年 <?= $month; ?> 月份</h1>
+  <a href="?y=<?= $nextYear?>&m=<?= $nextMonth; ?>">下一個月</a>
+</div>
+
+
+
+<div class='cal'>
+  <?php
+  
+      $week=array("mon","tue","wed","thu","fri","sat","sun");
+       for ($i=0; $i < 7; $i++) { 
+        echo "<div class=' week mt-2  '>";
+        echo $week[$i];
+        echo "<div>&nbsp</div>";
+        echo "</div>";
+       }
+      
+
     foreach ($cal as $i => $day) {
       if ($day != "") {
         $show = explode("-", $day)[2];
@@ -126,13 +166,16 @@ echo "</pre>"; */
 
       if (array_key_exists($day, $holiday)) {
 
-        echo "<div class='date holiday'>";
+        echo "<div class='date holiday '>";
         echo $show;
         echo "<div>{$holiday[$day]}</div>";
         echo "</div>";
       } else {
 
-        echo "<div class='date'>$show</div>";
+        echo "<div class='date'>";
+        echo $show;
+        echo "<div>&nbsp</div>";
+        echo "</div>";
       }
     }
 
