860d5369eb7ef9c9f90b787b37579ade1e5b3e51
abby <abby201205@gmail.com>
2022-12-01 16:05:57 +0800

    array

--- html/js/j20221201_1_array.php
+++ html/js/j20221201_1_array.php
@@ -24,7 +24,12 @@
 
     //Indexed Arrays
     $indexArr = ['a', 'b', 'c']; //key未指定時預設從0開始
+    echo '<hr>';
     dd($indexArr);
+    //指定回傳值
+    dd($indexArr[0]);
+    dd($indexArr[1]);
+    dd($indexArr[2]);
 
 
     //Associative Arrays
@@ -33,6 +38,11 @@
         's2' => 'abb',
         's3' => 'abby'
     ];
+    echo '<hr>';
+    //設定key值並回傳
+    dd($assocArr['s1']);
+    dd($assocArr['s2']);
+    dd($assocArr['s3']);
 
     //多維array
     //Multidimensional Arrays
@@ -45,35 +55,58 @@
         's2' => 'abb',
         's3' => 'abby'
     ]];
+    echo '<hr>';
     dd($multiArr);
 
     //選5隻手機
     //name price 組二維
     //北中南 location 三維
-    $mobiall=[[
+    $mobileArr = [[
         'name' => 'apple',
         'price' => '50000',
-    ],[
+        'location' => ['北', '中', '南'] //三維
+    ], [
         'name' => 'apple5s',
         'price' => '50000',
-    ],[
+        'location' => ['北', '中', '南']
+    ], [
         'name' => 'applexs',
         'price' => '50000',
-    ],[
+        'location' => ['北', '中', '南']
+    ], [
         'name' => 'apple13',
         'price' => '50000',
-    ],[
+        'location' => ['北', '中', '南']
+    ], [
         'name' => 'apple16',
         'price' => '50000',
-    ]
+        'location' => ['北', '中', '南']
+    ]];
+    dd($mobileArr); //function
 
-    ];
+    foreach ($mobileArr as $key => $value) {
+        echo '<hr>';
+        dd($value);
+    }
 
 
 
     ?>
 
     <script>
+        //array
+        //第一種宣告方式：語法
+        let arr=[
+            1,2,3
+        ];
+        console.log(arr);
+
+        //第二種：用function
+        let arr2 =new Array(4,5,6);
+        console.log(arr2);
+        console.log(arr2[0]);
+        console.log(arr2[1]);
+        console.log(arr2[2]);
 
     </script>
 
